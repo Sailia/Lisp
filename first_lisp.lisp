bf5d513f11556e97bc955d620e49bec09e6ae834
Skylar <syema.ailia@gmail.com>
2016-09-24 21:48:47 -0500

    add more function definitions

--- first_lisp.lisp
+++ first_lisp.lisp
@@ -46,6 +46,19 @@ Multiline comments
 (format t "(rem 5 6) = ~d ~%" (rem 5 6))
 (format t "(mod 5 6) = ~d ~%" (mod 5 6))
 
+(format t "(expt 4 2) = ~d ~%" (expt 4 2))
+(format t "(sqrt 81) = ~d ~%" (sqrt 81))
+(format t "(exp 1) = ~d ~%" (exp 1))
+(format t "(log 1000 10) = ~d ~%" (log 1000 10)) ; = 3 = Because 10^3 = 1000
+(format t "(eq 'dog 'dog) = ~d ~%" (eq 'dog 'dog))
+(format t "(floor 5.5) = ~d ~%" (floor 5.5))
+(format t "(ceiling 5.5) = ~d ~%" (ceiling 5.5))
+(format t "(max 5 10) = ~d ~%" (max 5 10))
+(format t "(min 5 10) ~d ~%" (min 5 10))
+(format t "(oddp 15) = ~d ~%" (oddp 15))
+(format t "(evenp 15) = ~d ~%" (evenp 15)) ; Nil also means false
+(format t "(numberp 2) = ~d ~%" (numberp 2))
+(format t "(null nil) = ~d ~%" (null nil))
 
 
 
